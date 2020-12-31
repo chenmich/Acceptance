b72fb4e07c108e79d5234ce8df1e97cd525db7b8
chenmich <403189920@qq.com>
2020-12-31 16:24:36 +0800

    complete the test for normal_data

--- helper/normal_data.py
+++ helper/normal_data.py
@@ -2,24 +2,19 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-def normal_data(mu=0, sigma=1): 
+def standard_normally_distribution(z:np.ndarray, mu=0, sigma=1): 
     '''
-        This fuction will return a pair 
-        of normal distribution fuction value.
+        This fuction will return the value normal distribution fuction.
         Arg:
-            mu:    the mean of 
-            sigma: the variance of distribution
+            z: standard normally distribution data point
         Return:
-            data_x : independent variable‘s value
-            data_y : dependent variable's value
+            data_x: the data point following the N(mu, sigma)
+            data_y: the density function value of the data_x
 
     '''
-    data_x = np.arange(mu - 3 * sigma, 
-                mu + 3 * sigma, step=0.2)#generate the x
-    data_y = data_x - mu   
-    data_y = np.power(data_y, 2)
-    data_y = data_y / (-2 * sigma * sigma)
-    data_y = np.exp(data_y)
-    data_y = data_y / math.sqrt(2 * 3.1415)
-    data_y = data_y /sigma
+    data_x = mu + z * sigma
+    data_y = np.power(z,2)
+    data_y = np.exp(-data_y / 2)
+    data_y = data_y /sigma / math.sqrt(2)
+    
     return data_x, data_y
